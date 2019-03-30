48dcae2f65a99d9b77e9080605664728780f4639
Mohamed Omane <omanem@union.edu>
2019-03-30 17:09:41 -0400

    Created Files for Preference and Main system

---
+++ SaintsHack-matchingAlgorithm/src/MatchingSystem.java
@@ -0,0 +1,6 @@
+public class MatchingSystem {
+    public static void main(String[] args) {
+        System.out.println("This is the MAIN file. This will be run once data is inputted. ");
+    }
+
+}
